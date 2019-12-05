475e1fdd3745b79cb96c9cf2f956704a6f67db14
Michael Lavelle <michael@lavelle.name>
2019-12-05 22:44:09 +0000

    Initial Demo with prototype / expermental implementation

---
+++ src/test/java/org/ml4j/InceptionV4DemoTests.java
@@ -0,0 +1,13 @@
+package org.ml4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InceptionV4DemoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
