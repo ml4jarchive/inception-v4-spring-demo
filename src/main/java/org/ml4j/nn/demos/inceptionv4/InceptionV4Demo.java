70772151487880f8ee190dcf2c54bd28b2001739
Michael Lavelle <michael@lavelle.name>
2019-12-12 16:20:13 +0000

    Refactoring for dataset abstraction

--- src/main/java/org/ml4j/nn/demos/inceptionv4/InceptionV4Demo.java
+++ src/main/java/org/ml4j/nn/demos/inceptionv4/InceptionV4Demo.java
@@ -1,25 +1,24 @@
 package org.ml4j.nn.demos.inceptionv4;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Path;
+import java.util.function.Supplier;
 import java.util.logging.LogManager;
-
-import javax.imageio.ImageIO;
+import java.util.stream.Stream;
 
 import org.ml4j.Matrix;
 import org.ml4j.MatrixFactory;
+import org.ml4j.images.Image;
 import org.ml4j.nn.FeedForwardNeuralNetworkContext;
 import org.ml4j.nn.ForwardPropagation;
-import org.ml4j.nn.demos.inceptionv4.util.BufferedImageFeaturesMapper;
+import org.ml4j.nn.datasets.BatchedDataSet;
+import org.ml4j.nn.datasets.featureextraction.ImageSupplierFeatureExtractor;
+import org.ml4j.nn.datasets.images.DirectoryImagesWithPathsDataSet;
+import org.ml4j.nn.datasets.images.LabeledImagesDataSet;
+import org.ml4j.nn.datasets.neuronsactivation.NeuronsActivationDataSet;
 import org.ml4j.nn.models.inceptionv4.InceptionV4Factory;
 import org.ml4j.nn.models.inceptionv4.InceptionV4Labels;
-import org.ml4j.nn.neurons.ImageNeuronsActivationImpl;
-import org.ml4j.nn.neurons.Neurons3D;
 import org.ml4j.nn.neurons.NeuronsActivation;
-import org.ml4j.nn.neurons.NeuronsActivationFeatureOrientation;
 import org.ml4j.nn.supervised.FeedForwardNeuralNetworkContextImpl;
 import org.ml4j.nn.supervised.SupervisedFeedForwardNeuralNetwork;
 import org.slf4j.Logger;
@@ -49,67 +48,66 @@ public class InceptionV4Demo implements CommandLineRunner {
 		// Application entry-point
 		SpringApplication.run(InceptionV4Demo.class, args);
 	}
-
+	
 	@Override
 	public void run(String... args) throws Exception {
-
+	
+		// Create the data set
+		
+		// Define images Directory
+		Path imagesDirectory = new File(InceptionV4Demo.class.getClassLoader().getResource("test_images").getFile()).toPath();
+		
+		// Define data set of images from a directory labelled with the path names
+		LabeledImagesDataSet<String> labeledImagesDataSet = 
+							new DirectoryImagesWithPathsDataSet(imagesDirectory, 
+									path -> true)
+								.getLabeledImagesDataSet((index, pathName) -> 
+									pathName.getParent().getFileName().toString());
+			
+		// Map to a data set of image batches with more than 2 in each batch
+		BatchedDataSet<Supplier<Image>> batchedImageSupplierDataSet = labeledImagesDataSet.getDataSet().toBatchedDataSet(2);
+		
+		// Map to a data set of NeuronsActivation instances, one for each batch
+		NeuronsActivationDataSet neuronsActivationDataSet = batchedImageSupplierDataSet.toNeuronsActivationDataSet(matrixFactory, 
+				new ImageSupplierFeatureExtractor(299 * 299 * 3));
+		
+		
+		// Create the prediction context and the neural network
+		
 		// Create a runtime (non-training) context for the Inception V4 Network
 		FeedForwardNeuralNetworkContext predictionContext = new FeedForwardNeuralNetworkContextImpl(matrixFactory, false);
-		predictionContext.setTrainingMiniBatchSize(2);
 		
 		// Create the Inception V4 Network, configuring the prediction context
 		SupervisedFeedForwardNeuralNetwork inceptionV4Network = inceptionV4Factory.createInceptionV4(predictionContext);
+	
+		// Make the predictions
 		
-		// Create the neuron activations for the input image
-		NeuronsActivation imageNeuronsActivation = getImageActivations();
-
-		LOGGER.info("Forward propagating images through Inception V4...");
+		Stream<ForwardPropagation> forwardPropagations = inceptionV4Network
+				.forwardPropagate(neuronsActivationDataSet.stream(), predictionContext);
 		
-		// Forward propagate the image neurons activation through the Inception V4
-		// Network
-		ForwardPropagation result = inceptionV4Network.forwardPropagate(imageNeuronsActivation, predictionContext);
+		// Output the predictions
 		
-		LOGGER.info("Obtained predictions");
-
-		// Obtain the predictions for the batch from the result
-		Matrix classificationProbablitiesForBatch = result.getOutput().getActivations(matrixFactory);
 		
 		// Obtain the labels for the InceptionV4 Network
 		InceptionV4Labels inceptionV4ClassificationNames = inceptionV4Factory.createInceptionV4Labels();
-
+	
+		// Output the predictions for each batch
+		forwardPropagations.forEach(fp -> outputBatchPredictions(fp.getOutput(), inceptionV4ClassificationNames));
+		
+		
+	}
+	
+	private void outputBatchPredictions(NeuronsActivation outputActivation, InceptionV4Labels inceptionV4ClassificationNames){
+		Matrix classificationProbablitiesForBatch = outputActivation.getActivations(matrixFactory);
+		
 		for (int column = 0; column < classificationProbablitiesForBatch.getColumns(); column++) {
 			Matrix classificationProbablitiesForExample = classificationProbablitiesForBatch.getColumn(column);
 			int predictedIndex = classificationProbablitiesForExample.argmax();
 			String predictedClassificationName = inceptionV4ClassificationNames.getLabel(predictedIndex);
-			LOGGER.info("Predicted : '" + predictedClassificationName + "' with probability " + classificationProbablitiesForExample.get(predictedIndex));
+			LOGGER.info("Predicted : '" + predictedClassificationName + "' with probability "
+					+ classificationProbablitiesForExample.get(predictedIndex));
 		}
 	}
 	
-	private NeuronsActivation getImageActivations(List<BufferedImage> bufferedImages)  {
-		List<float[]> imagesList = new ArrayList<>();
-		BufferedImageFeaturesMapper mapper = new BufferedImageFeaturesMapper(299 , 299);
-
-		for (BufferedImage bufferedImage : bufferedImages){
-			imagesList.add(mapper.toFeaturesVector(bufferedImage));
-		}
-		float[][] images = new float[imagesList.size()][mapper.getFeatureCount()];
-		int i = 0;
-		for (float[] image : imagesList) {
-			images[i++] = image;
-		}
-		return new ImageNeuronsActivationImpl(matrixFactory.createMatrixFromRows(images).transpose(), new Neurons3D(299, 299, 3, false), 
-				NeuronsActivationFeatureOrientation.ROWS_SPAN_FEATURE_SET, false);
-	}
 	
-	
-	private NeuronsActivation getImageActivations() throws IOException {
-		File imagesDirectory = new File(InceptionV4Demo.class.getClassLoader().getResource("test_images").getFile());
-		List<BufferedImage> bufferedImages = new ArrayList<>();
-		for (File imageFile : imagesDirectory.listFiles(f -> f.getPath().endsWith(".jpg"))) {
-			LOGGER.info("Loaded image:" + imageFile.getName());
-			BufferedImage bufferedImage = ImageIO.read(imageFile);		
-			bufferedImages.add(bufferedImage);
-		}
-		return getImageActivations(bufferedImages);
-	}
 }
